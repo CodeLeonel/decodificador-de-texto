e13d200884c7cedae35d69f421caa8dd47e27436
Hugo Leonel <hugoleonel.code@gmail.com>
2024-01-29 01:46:27 -0300

    Validar e Copiar

--- scripts/script.js
+++ scripts/script.js
@@ -8,17 +8,11 @@ function criptografar() {
         texto = texto.replaceAll('a','ai');
         texto = texto.replaceAll('o','ober');
         texto = texto.replaceAll('u','ufat');
+
+        exibeTexto(texto);
+    } else {
+        exibeApresentacaoSemConteudo();
     }
-
-    let elementoSaidaTexto = document.getElementsByClassName('apresentacao__saida__texto').item(0);
-    elementoSaidaTexto.innerHTML = texto;
-
-    let imagem = document.getElementsByClassName('apresentacao__saida__imagem').item(0);
-    let mensagens = document.getElementsByClassName('apresentacao__saida__mensagens');
-    mensagens.item(0).style.display = 'none';
-    imagem.style.display = 'none';
-
-    elementoSaidaTexto.style.display = 'block';
 
 }
 
@@ -32,16 +26,80 @@ function descriptografar() {
         texto = texto.replaceAll('ai','a');
         texto = texto.replaceAll('ober','o');
         texto = texto.replaceAll('ufat','u');
+
+        exibeTexto(texto);
+    } else {
+        exibeApresentacaoSemConteudo();
     }
 
-    let elementoSaidaTexto = document.getElementsByClassName('apresentacao__saida__texto').item(0);
+}
+
+function exibeTexto(texto) {
+
+    let elementoSaidaTexto = document.getElementsByClassName('apresentacao__saida__comconteudo__texto').item(0);
     elementoSaidaTexto.innerHTML = texto;
 
-    let imagem = document.getElementsByClassName('apresentacao__saida__imagem').item(0);
-    let mensagens = document.getElementsByClassName('apresentacao__saida__mensagens');
-    mensagens.item(0).style.display = 'none';
-    imagem.style.display = 'none';
+    let apresentacao_sem_conteudo = document.getElementById('saida__semconteudo');
+    apresentacao_sem_conteudo.style.display = 'none';
 
-    elementoSaidaTexto.style.display = 'block';
+    let apresentacao_com_conteudo = document.getElementById('saida__comconteudo');
+    apresentacao_com_conteudo.style.display = 'flex';
+
+}
+
+function exibeApresentacaoSemConteudo() {
+
+    let apresentacao_sem_conteudo = document.getElementById('saida__semconteudo');
+    apresentacao_sem_conteudo.style.display = 'flex';
+
+    let apresentacao_com_conteudo = document.getElementById('saida__comconteudo');
+    apresentacao_com_conteudo.style.display = 'none';
+
+}
+
+function validacao(event) {
+
+    let textoValido = /^[a-z]+$/.test(event.value) || event.value == "";
+
+    if(!textoValido) {
+        alerta(event);
+    } else {
+        normal(event);
+    }
+    
+    
+}
+
+function alerta(event) {
+
+    event.style.border = '1px solid var(--cor-alerta)';
+    document.getElementById('criptografar').setAttribute('disabled',true);
+    document.getElementById('criptografar').style.setProperty('background-color','var(--cor-quaternaria)');
+    document.getElementById('criptografar').style.setProperty('color','var(--cor-ternaria)');
+    document.getElementById('descriptografar').setAttribute('disabled',true);
+    document.getElementById('descriptografar').style.setProperty('background-color','var(--cor-terciaria)');
+    document.getElementById('descriptografar').style.setProperty('color','var(--cor-quaternaria)');
+    document.getElementById('texto_alerta').style.setProperty('color','var(--cor-alerta)');
+
+}
+
+function normal(event) {
+
+    event.style.border = 'none';
+    document.getElementById('criptografar').removeAttribute('disabled');
+    document.getElementById('criptografar').style.setProperty('background-color','var(--cor-secundaria)');
+    document.getElementById('criptografar').style.setProperty('color','var(--cor-primaria)');
+    document.getElementById('descriptografar').removeAttribute('disabled');
+    document.getElementById('descriptografar').style.setProperty('background-color','var(--cor-primaria)');
+    document.getElementById('descriptografar').style.setProperty('color','var(--cor-secundaria)');
+    document.getElementById('texto_alerta').style.setProperty('color','var(--cor-terciaria)');
+    
+}
+
+function copiar() {
+
+    let conteudoSaidaTexto = document.getElementsByClassName('apresentacao__saida__comconteudo__texto').item(0).innerHTML;
+
+    navigator.clipboard.writeText(conteudoSaidaTexto);
 
 }
